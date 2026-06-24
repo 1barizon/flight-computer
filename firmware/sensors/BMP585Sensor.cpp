d0d4b9dc5d18c85e8543797909998992d85ff813
1barizon <joao.mello@grad.iprj.uerj.br>
2026-06-24 11:08:55 -0300

    divisao por 1000 volta

--- firmware/sensors/BMP585Sensor.cpp
+++ firmware/sensors/BMP585Sensor.cpp
@@ -75,7 +75,7 @@ void BMP585Sensor::update() {
   temperature = _bmp.temperature;
   altitude = current_altitude;
 
-  const float dt = (current_time - prev_time);
+  const float dt = (current_time - prev_time) / 1000.0F;
   if (dt > 0.001F) {
     float vz = (current_altitude - prev_altitude) / dt;
 
